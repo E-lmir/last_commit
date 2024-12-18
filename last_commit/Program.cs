f4cb86f8dcc216aa7050d29891974eda3f812789
Габтрахманов Эльмир (Gabtrakhmanov_ER) <Gabtrakhmanov_ER@directum.ru>
2024-12-18 15:09:18 +0400

    Add method

--- last_commit/Program.cs
+++ last_commit/Program.cs
@@ -5,26 +5,37 @@ namespace last_commit
 {
   internal class Program
   {
+    private static string _drive;
+    private static string _repos;
+    private static CommandLineExecutor _executor;
+
     static void Main(string[] args)
     {
-      var repos = args.First(x => x.Contains("repos=")).Split('=').Last();
+      _repos = args.First(x => x.Contains("repos=")).Split('=').Last();
       var date = args.First(x => x.Contains("date=")).Split('=').Last();
       var remote = args.FirstOrDefault(x => x.Contains("remote="))?.Split('=').Last() ?? "origin";
       var path = args.FirstOrDefault(x => x.Contains("output="))?.Split('=').Last() ?? "branches.md";
 
       File.Delete(path);
 
-      var drive = repos.Split(':').FirstOrDefault();
-      var executor = new CommandLineExecutor();
-      var result = executor.Execute($"{drive}:", $"cd {repos}", $"git fetch -P --all");
-      var branches = executor.Execute($"{drive}:", $"cd {repos}", "git branch -r -f").Split().Where(x => x.Contains(remote));
+      _drive = _repos.Split(':').FirstOrDefault()!;
+      _executor = new CommandLineExecutor();
+
+      ExecuteGitCommand($"git fetch -p -a");
+      var branches = ExecuteGitCommand("git branch -r -f").Split().Where(x => x.Contains(remote));
+      var noMerghedInMasterBranches = ExecuteGitCommand("git checkout master && git branch -r -f --no-merged").Split().Where(x => x.Contains(remote));
+
       foreach (var branch in branches)
       {
-        var lastCommit = executor.Execute($"{drive}:", $"cd {repos}", $"git log --pretty=format:\"%ae;%ad\" --date=short -n 1 {branch}").Split(Environment.NewLine)[8].Split(';');
+        var lastCommit = ExecuteGitCommand($"git log --pretty=format:\"%ae;%ad\" --date=short -n 1 {branch}").Split(Environment.NewLine)[8].Split(';');
         var message = $"branch:{branch}\tlast commited:{lastCommit[0]}\tat:{lastCommit[^1]}";
         if (DateTime.Parse(lastCommit[^1]) < DateTime.Parse(date))
         {
           Console.ForegroundColor = ConsoleColor.Green;
+
+          if (noMerghedInMasterBranches.Contains(branch))
+            message += "\t NO-MERGED";
+
           File.AppendAllText(path, message + Environment.NewLine, Encoding.UTF8);
         }
 
@@ -32,5 +43,7 @@ namespace last_commit
         Console.ForegroundColor = ConsoleColor.White;
       }
     }
+
+    private static string ExecuteGitCommand(string command) => _executor.Execute($"{_drive}:", $"cd {_repos}", command);
   }
 }
